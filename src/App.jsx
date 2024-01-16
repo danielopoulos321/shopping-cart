79201b53e0a811c3cfa3f0cb094a441e04912606
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-15 21:53:15 -0500

    Refactor to use Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
+import styles from "./App.module.css";
 
 function App() {
-  const cartItems = [0, 0, 0];
+  const cartItems = [0, 0, 0, 0];
   return (
-    <>
+    <div className={styles.container}>
       <Navbar cartItems={cartItems} />
       <Outlet />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
 export function Navbar({ cartItems }) {
   const cartLength = cartItems.length; // Change to account for multiples of an item with reduce
   return (
     <>
-      <div className="navContainer">
+      <div className={styles.navContainer}>
         <h1>Company</h1>
         <Link to="/">Home</Link>
         <Link to="shop">Shop</Link>
