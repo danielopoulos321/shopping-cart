60b813109e9e2e6409773a645c83c9c712e35d92
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-16 09:15:08 -0500

    Provide Outlet Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
+import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  const cartItems = [0, 0, 0, 0];
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div className={styles.container}>
       <Navbar cartItems={cartItems} />
-      <Outlet />
+      <Outlet context={[cartItems, setCartItems]} />
     </div>
   );
 }
