a62ec21b1f4f1d331edbe9333a624e03b0d1319f
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-16 09:15:38 -0500

    Prelim add item functionality

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,3 +1,18 @@
+import { useOutletContext } from "react-router-dom";
+
+/* eslint-disable react/prop-types */
 export default function Shop() {
-  return <div>Shop</div>;
+  const [cartItems, setCartItems] = useOutletContext();
+  const addItem = () => {
+    console.log(cartItems);
+    let newItemArr = [...cartItems];
+    newItemArr.push(0);
+    setCartItems(newItemArr);
+  };
+  return (
+    <div>
+      <h2>Shop</h2>
+      <button onClick={addItem}>Add Item</button>
+    </div>
+  );
 }
