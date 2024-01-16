494f5792cb3065d011b3c8e9d2fb5605929a3618
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-15 21:53:25 -0500

    Corrected imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Shop from "./components/Shop.jsx";
-import Home from "./components/Home.jsx";
+import Shop from "./components/Shop/Shop.jsx";
+import Home from "./components/Home/Home.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
