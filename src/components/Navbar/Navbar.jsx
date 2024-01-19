65a012ff1a8f8e7ab5a9f7957b8c104bb155d60a
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-19 13:37:23 -0500

    Add cart navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,8 @@ export function Navbar({ cartItems }) {
     <header className={styles.navContainer}>
       <h1>Company</h1>
       <Link to="/">Home</Link>
-      <Link to="shop">Shop</Link>
+      <Link to="/shop">Shop</Link>
+      <Link to="/cart">Cart</Link>
       <h2>Items: {cartLength}</h2>
     </header>
   );
