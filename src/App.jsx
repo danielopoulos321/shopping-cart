b923d190911febb123605ce38fc2e4dd65742b76
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-19 11:52:20 -0500

    Data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
 import styles from "./App.module.css";
 import Shop from "./components/Shop/Shop.jsx";
 
 function App() {
+  const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  });
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
   const router = createBrowserRouter([
     {
       index: true,
