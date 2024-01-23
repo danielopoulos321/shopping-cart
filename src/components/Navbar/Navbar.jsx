062d13fbfc2c1069dc928bc821a04178a9c630df
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-22 23:03:25 -0500

    Change nav link positions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,11 +8,14 @@ export function Navbar({ cartItems }) {
   }, 0);
   return (
     <header className={styles.navContainer}>
-      <h1>Company</h1>
-      <Link to="/">Home</Link>
-      <Link to="/shop">Shop</Link>
-      <Link to="/cart">Cart</Link>
-      <h2>Items: {cartLength}</h2>
+      <h1>FakeStore</h1>
+      <div className={styles.links}>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/cart">
+          <h2 className={styles.cart}>Cart ({cartLength})</h2>
+        </Link>
+      </div>
     </header>
   );
 }
