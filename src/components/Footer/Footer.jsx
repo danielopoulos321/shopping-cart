e8cf15b373b0965ca2f8721035f66a518273c08f
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-22 22:32:22 -0500

    Add stock footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import "./Footer.module.css";
+export default function Footer() {
+  return (
+    <footer>
+      <p>Daniel Anestopoulos</p>
+    </footer>
+  );
+}
