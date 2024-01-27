30647497711bec290001998ebfd23f51f1e0f22f
Daniel Anestopoulos <dstops97@gmail.com>
2024-01-26 23:18:42 -0500

    Fix Footer placement

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 import { Navbar } from "../Navbar/Navbar";
-
+import Footer from "../Footer/Footer";
+import styles from "./Home.module.css";
 export default function Home({ cartItems }) {
   return (
-    <>
+    <div className={styles.container}>
       <Navbar cartItems={cartItems} />
-      <h1>Home</h1>
-    </>
+      <Footer />
+    </div>
   );
 }
